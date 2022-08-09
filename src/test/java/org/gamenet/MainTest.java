e7a2f16125b3b6f2dc10989a039b68c82672bee6
Mike Kienenberger <mkienenb@gmail.com>
2022-08-09 18:39:07 -0400

    test: passed test of framework

---
+++ src/test/java/org/gamenet/MainTest.java
@@ -0,0 +1,12 @@
+package org.gamenet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void applesauce() {
+        return;
+    }
+}
