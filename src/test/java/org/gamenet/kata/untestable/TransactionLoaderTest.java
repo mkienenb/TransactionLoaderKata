d01fa6b7af4088390bd851f918fac86cfca9f430
Michael R. Wolf <MichaelRWolf@att.net>
2022-08-23 18:39:50 -0400

    refactor - optimize imports

--- src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
+++ src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
@@ -1,10 +1,6 @@
 package org.gamenet.kata.untestable;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionLoaderTest {
 
