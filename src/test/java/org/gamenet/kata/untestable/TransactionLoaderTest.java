d69bef835f83a1ac86f6b6165761ec3cb45da2ed
Michael R. Wolf <MichaelRWolf@att.net>
2022-08-23 19:27:25 -0400

    test - Call init with (very short) valid XML.

--- src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
+++ src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
@@ -2,12 +2,20 @@ package org.gamenet.kata.untestable;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class TransactionLoaderTest {
 
 
     @Test
     void applesauce() {
         TransactionLoader tl = new TransactionLoader();
-        tl.init("<xxxx/>");
+        String xml_string = "<items/>\n";
+
+        tl.init(xml_string);
+
+        assertTrue(tl.includeItems);
+        assertFalse(tl.includeCancelled);
     }
 }
