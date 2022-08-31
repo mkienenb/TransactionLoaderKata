3b85b81286c265b7985955ecf7c91f1effeb5b37
Mike Kienenberger <mkienenb@gmail.com>
2022-08-31 17:15:40 -0400

    refactor: use local variable instead of field for xml string

--- src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
+++ src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
@@ -1,6 +1,5 @@
 package org.gamenet.kata.untestable;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -8,16 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TransactionLoaderTest {
 
-    private String xml_string;
+    private String xml_string1;
 
-    @BeforeEach
-    void setUp() {
-        xml_string = "<items/>\n";
-    }
     @Test
     void shouldNoticeItemsAndNoCancelledInXmlStream() {
         TransactionLoader tl = new TransactionLoader();
-
+        String xml_string = "<items/>\n";
         tl.init(xml_string);
 
         assertTrue(tl.includeItems);
