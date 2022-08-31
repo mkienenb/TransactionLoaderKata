1c4a234f262a3991309f0c45b3f9eab3a79a7d09
Mike Kienenberger <mkienenb@gmail.com>
2022-08-31 18:05:43 -0400

    refactor: extracted applesauce() method that exposes the ByteArrayOutputStream

--- src/main/java/org/gamenet/kata/untestable/TransactionLoader.java
+++ src/main/java/org/gamenet/kata/untestable/TransactionLoader.java
@@ -32,63 +32,67 @@ public class TransactionLoader {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
 
         try {
-            XMLStreamWriter writer = XMLOutputFactory.newInstance().createXMLStreamWriter(
-                    new OutputStreamWriter(os, "utf-8"));
-            writer.writeStartElement("res");
-            writer.writeStartElement("transactions");
-            try (Connection tConn = getConnection("trandb");
-                 Connection cConn = getConnection("custdb")) {
-                PreparedStatement ps = tConn.prepareStatement("select customer_id, order_id, price, status, from transaction where status != 'cancelled'");
-                if (includeCancelled) {
-                    ps = tConn.prepareStatement("select customer_id, order_id, price, status, from transaction");
-                }
-                ResultSet rs = ps.executeQuery();
-                ResultSet rs2;
-                while (rs.next()) {
-                    writer.writeStartElement("t");
-                    writer.writeAttribute("s", rs.getString(4).toUpperCase());
-                    ps = cConn.prepareStatement("select name from customer where id = ?");
-                    ps.setInt(1, rs.getInt("customer_id"));
-                    ResultSet crs = ps.executeQuery();
-                    if (!crs.next()) {
-                        writer.writeStartElement("n");
-                        writer.writeCharacters("UNK");
-                        writer.writeEndElement();
-                    } else {
-                        writer.writeStartElement("n");
-                        writer.writeCharacters(crs.getString(1));
-                        writer.writeEndElement();
-                    }
-                    ps = tConn.prepareStatement("select name, category from order_item where order_id = ?");
-                    ps.setInt(1, rs.getInt(2));
-                    rs2 = ps.executeQuery();
-                    if (includeItems) {
-                        while (rs2.next()) {
-                            writer.writeStartElement("i");
-                            writer.writeAttribute("name", rs2.getString(1));
-                            writer.writeAttribute("cat", rs2.getString(2).toUpperCase());
-                            writer.writeEndElement();
-                        }
-                    }
-                    rs.getString("order_id");
-                    writer.writeStartElement("p");
-                    writer.writeCharacters(new DecimalFormat("0.00").format(rs.getInt("price") / 100.0));
-                    writer.writeEndElement();
-                    writer.writeEndElement();
-                }
-                rs.close();
-            }
-            writer.writeEndElement();
-            writer.writeEndElement();
-            writer.writeEndDocument();
-            writer.close();
-            return os.toString("utf-8");
+            return applesauce(os);
         } catch (XMLStreamException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         return "<error/>";
+    }
+
+    String applesauce(ByteArrayOutputStream os) throws XMLStreamException, UnsupportedEncodingException, SQLException {
+        XMLStreamWriter writer = XMLOutputFactory.newInstance().createXMLStreamWriter(
+                new OutputStreamWriter(os, "utf-8"));
+        writer.writeStartElement("res");
+        writer.writeStartElement("transactions");
+        try (Connection tConn = getConnection("trandb");
+             Connection cConn = getConnection("custdb")) {
+            PreparedStatement ps = tConn.prepareStatement("select customer_id, order_id, price, status, from transaction where status != 'cancelled'");
+            if (includeCancelled) {
+                ps = tConn.prepareStatement("select customer_id, order_id, price, status, from transaction");
+            }
+            ResultSet rs = ps.executeQuery();
+            ResultSet rs2;
+            while (rs.next()) {
+                writer.writeStartElement("t");
+                writer.writeAttribute("s", rs.getString(4).toUpperCase());
+                ps = cConn.prepareStatement("select name from customer where id = ?");
+                ps.setInt(1, rs.getInt("customer_id"));
+                ResultSet crs = ps.executeQuery();
+                if (!crs.next()) {
+                    writer.writeStartElement("n");
+                    writer.writeCharacters("UNK");
+                    writer.writeEndElement();
+                } else {
+                    writer.writeStartElement("n");
+                    writer.writeCharacters(crs.getString(1));
+                    writer.writeEndElement();
+                }
+                ps = tConn.prepareStatement("select name, category from order_item where order_id = ?");
+                ps.setInt(1, rs.getInt(2));
+                rs2 = ps.executeQuery();
+                if (includeItems) {
+                    while (rs2.next()) {
+                        writer.writeStartElement("i");
+                        writer.writeAttribute("name", rs2.getString(1));
+                        writer.writeAttribute("cat", rs2.getString(2).toUpperCase());
+                        writer.writeEndElement();
+                    }
+                }
+                rs.getString("order_id");
+                writer.writeStartElement("p");
+                writer.writeCharacters(new DecimalFormat("0.00").format(rs.getInt("price") / 100.0));
+                writer.writeEndElement();
+                writer.writeEndElement();
+            }
+            rs.close();
+        }
+        writer.writeEndElement();
+        writer.writeEndElement();
+        writer.writeEndDocument();
+        writer.close();
+        return os.toString("utf-8");
     }
 
     private Connection getConnection(String dbName) throws SQLException {
