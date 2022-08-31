d0d010123e95108976f61a3e4130acb2976c18d1
Michael R. Wolf <MichaelRWolf@att.net>
2022-08-31 17:03:14 -0400

    refactor - Move XML string setup outside test (for reuse later).

--- src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
+++ src/test/java/org/gamenet/kata/untestable/TransactionLoaderTest.java
@@ -1,5 +1,6 @@
 package org.gamenet.kata.untestable;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -7,11 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TransactionLoaderTest {
 
+    private String xml_string;
 
+    @BeforeEach
+    void setUp() {
+        xml_string = "<items/>\n";
+    }
     @Test
     void shouldNoticeItemsAndNoCancelledInXmlStream() {
         TransactionLoader tl = new TransactionLoader();
-        String xml_string = "<items/>\n";
 
         tl.init(xml_string);
 
