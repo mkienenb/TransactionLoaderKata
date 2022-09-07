384188ac543d68b8d53659bedada07850310f823
Mike Kienenberger <mkienenb@gmail.com>
2022-09-07 18:11:26 -0400

    chore: added mockito test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("com.approvaltests:approvaltests:18.0.0")
+    testImplementation("org.mockito:mockito-core:4.8.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
